eddc5dd4a2b3b46f125847cf0115e6162c0a9ec0
MackoSSS <marcin.mackowiak@windowslive.com>
2016-04-21 15:39:03 +0200

    Added files via upload

--- main.c
+++ main.c
@@ -1,78 +1,135 @@
-#include "stm32f4xx_rcc.h"
-#include "stm32f4xx_spi.h"
-#include "stm32f4xx_gpio.h"
-#include "stm32f4xx_i2c.h"
-
-uint8_t spi_sendrecv(uint8_t byte)
-{
-	while (SPI_I2S_GetFlagStatus(SPI3, SPI_FLAG_TXE) == RESET);
-	SPI_I2S_SendData(SPI3, byte);
-
-	while(SPI_I2S_GetFlagStatus(SPI3, SPI_I2S_FLAG_RXNE) == RESET);
-	return SPI_I2S_ReceiveData(SPI3);
-}
-
+#include "defines.h"
+#include "stm32f4xx.h"
+#include "tm_stm32f4_delay.h"
+#include "tm_stm32f4_disco.h"
+#include "tm_stm32f4_fatfs.h"
+#include <stdio.h>
+#include <string.h>
+#include <main.h>
+#include <codec.h>
+#include <codec.c>
 
 int main(void)
 {
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOB | RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOD, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1 | RCC_APB1Periph_SPI3, ENABLE);
-	RCC_PLLI2SCmd(ENABLE);
+    //Fatfs object
+    FATFS FatFs;
+    //File object
+    FIL fil;
+    //Free and total space
+    uint32_t total, free;
+    char buff[30];
+    unsigned int ByteToRead = sizeof(buff);
+    unsigned int ByteRead;
+    //Initialize system
+    SystemInit();
+    //Initialize delays
+    TM_DELAY_Init();
+    //Initialize LEDs
+    TM_DISCO_LedInit();
+    fir_8 filt;
 
-	GPIO_InitTypeDef PinInitStruct;
-	PinInitStruct.GPIO_Pin = GPIO_Pin_4;
-	PinInitStruct.GPIO_Mode = GPIO_Mode_OUT;
-	PinInitStruct.GPIO_OType = GPIO_OType_PP;
-	PinInitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
-	PinInitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    //enables GPIO clock for PortD
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-	GPIO_Init(GPIOD, &PinInitStruct);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource4, GPIO_AF_SPI3);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
-	I2S_InitTypeDef I2S_InitType;
-	I2S_InitType.I2S_AudioFreq = I2S_AudioFreq_48k;
-	I2S_InitType.I2S_MCLKOutput = I2S_MCLKOutput_Enable;
-	I2S_InitType.I2S_Mode = I2S_Mode_MasterTx;
-	I2S_InitType.I2S_DataFormat = I2S_DataFormat_16b;
-	I2S_InitType.I2S_Standard = I2S_Standard_Phillips;
-	I2S_InitType.I2S_CPOL = I2S_CPOL_Low;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-	I2S_Init(SPI3, &I2S_InitType);
-	I2S_Cmd(SPI3, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 
-	I2C_InitTypeDef I2C_InitType;
-	I2C_InitType.I2C_ClockSpeed = 100000;
-	I2C_InitType.I2C_Mode = I2C_Mode_I2C;
-	I2C_InitType.I2C_OwnAddress1 = 99;
-	I2C_InitType.I2C_Ack = I2C_Ack_Enable;
-	I2C_InitType.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-	I2C_InitType.I2C_DutyCycle = I2C_DutyCycle_2;
+    codec_init();
+    codec_ctrl_init();
 
-	I2C_Init(I2C1, &I2C_InitType);
-	I2C_Cmd(I2C1, ENABLE);
+    I2S_Cmd(CODEC_I2S, ENABLE);
 
+    initFilter(&filt);
 
-	SystemInit();
+    //Mount drive
+    if (f_mount(&FatFs, "", 1) == FR_OK)
+    {
+        //Mounted OK, turn on RED LED
+        TM_DISCO_LedOn(LED_RED);
+        //Try to open file
+        if (f_open(&fil, "1stfile.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) == FR_OK)
+        {
+            //File opened, turn off RED and turn on GREEN led
+            TM_DISCO_LedOn(LED_GREEN);
+            TM_DISCO_LedOff(LED_RED);
+            f_read(&fil,buff,ByteToRead,&ByteRead);
 
-		fir_8 filt;
+            f_close(&fil);
+        }
+        f_mount(0, "", 1);
+    }
 
-		//enables GPIO clock for PortD
-		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    while(1)
+        {
 
-		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
-		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-		GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+        	if (SPI_I2S_GetFlagStatus(CODEC_I2S, SPI_I2S_FLAG_TXE))
+        	{
+        		SPI_I2S_SendData(CODEC_I2S, sample);
 
-		GPIO_Init(GPIOD, &GPIO_InitStructure);
+        		//only update on every second sample to insure that L & R ch. have the same sample value
+        		if (sampleCounter & 0x00000001)
+        		{
+        			sawWave += NOTEFREQUENCY;
+        			if (sawWave > 1.0)
+        				sawWave -= 2.0;
 
-		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+        			filteredSaw = updateFilter(&filt, sawWave);
 
-		codec_init();
-		codec_ctrl_init();
+        			sample = (int16_t)(NOTEAMPLITUDE*filteredSaw);
+        		}
+        		sampleCounter++;
+        	}
 
-		I2S_Cmd(CODEC_I2S, ENABLE);
+        	if (sampleCounter==48000)
+        	{
+        		LED_BLUE_OFF;
 
-		initFilter(&filt);
+        	}
+        	else if (sampleCounter == 96000)
+        	{
+        		LED_BLUE_ON;
+        		sampleCounter = 0;
+        	}
+
+        }
+    }
+
+    // a very crude FIR lowpass filter
+float updateFilter(fir_8* filt, float val)
+{
+    uint16_t valIndex=0;
+    uint16_t paramIndex;
+    float outval = 0.0;
+
+    valIndex = filt->currIndex;
+    filt->tabs[valIndex] = val;
+
+    for (paramIndex=0; paramIndex<10; paramIndex++)
+    {
+    	outval += (filt->params[paramIndex]) * (filt->tabs[(valIndex+paramIndex)&0x07]); //??
+    }
+
+    valIndex++;
+    //valIndex &= 0x07; ??
+
+    filt->currIndex = valIndex;
+
+    return outval;
+}
+
+void initFilter(fir_8* theFilter)
+{
+    uint8_t i,k=0.1;
+
+    theFilter->currIndex = 0;
+
+    for (i=0; i<10; i++)
+    	theFilter->tabs[i] += k;
 
 }
