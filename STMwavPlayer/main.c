c78a54eac2c3afc2e3a061bab2c1e1666ac304df
MackoSSS <marcin.mackowiak@windowslive.com>
2016-05-17 00:48:32 +0200

    odczyt dźwięku z karty
    odczyt pojedyńczych próbek

--- STMwavPlayer/main.c
+++ STMwavPlayer/main.c
@@ -1,3 +1,12 @@
+/*	Features:
+ 	 - FatFS by Chan supported, version RO. 11
+ 	 "tm_stm32f4_delay.h"
+ 	 "tm_stm32f4_disco.h"	Read SD card with FatFs by Tilen Majerle
+ 	 "tm_stm32f4_fatfs.h"
+
+ 	 "codec.h" and "codec.c"
+*/
+
 #include "defines.h"
 #include "stm32f4xx.h"
 #include "tm_stm32f4_delay.h"
@@ -8,44 +17,43 @@
 #include <main.h>
 #include <codec.h>
 #include <codec.c>
+#include <stm32f4xx_dma.h>
+#include <stm32f4xx_spi.h>
+#include <stm32f4xx_rcc.h>
 
 int main(void)
 {
-    //Fatfs object
+
+	//Fatfs object
     FATFS FatFs;
     //File object
     FIL fil;
-    //Free and total space
-    uint32_t total, free;
-    char buff[30];
-    unsigned int ByteToRead = sizeof(buff);
-    unsigned int ByteRead;
+    char sample1[3];
+    unsigned int ByteToRead = 45;
+    unsigned int ByteRead, i;
     //Initialize system
     SystemInit();
     //Initialize delays
     TM_DELAY_Init();
     //Initialize LEDs
     TM_DISCO_LedInit();
-    fir_8 filt;
-
-    //enables GPIO clock for PortD
+    int s;
+    int tab[45]={0};
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
+    	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
+    	GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+    	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 
-    codec_init();
-    codec_ctrl_init();
+    	codec_init();
+    	codec_ctrl_init();
 
-    I2S_Cmd(CODEC_I2S, ENABLE);
-
-    initFilter(&filt);
+    	I2S_Cmd(CODEC_I2S, ENABLE);
 
     //Mount drive
     if (f_mount(&FatFs, "", 1) == FR_OK)
@@ -53,83 +61,32 @@ int main(void)
         //Mounted OK, turn on RED LED
         TM_DISCO_LedOn(LED_RED);
         //Try to open file
-        if (f_open(&fil, "1stfile.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) == FR_OK)
+        if (f_open(&fil, "file.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) == FR_OK)
         {
             //File opened, turn off RED and turn on GREEN led
             TM_DISCO_LedOn(LED_GREEN);
             TM_DISCO_LedOff(LED_RED);
-            f_read(&fil,buff,ByteToRead,&ByteRead);
-
+            while(1)
+            {
+            	for(i=0; i<ByteToRead; i++)
+            	{
+            		f_read(&fil,sample1,3,&ByteRead);
+            		s = atoi(sample1);
+            		tab[i] = s;
+            	}
+            	if (SPI_I2S_GetFlagStatus(CODEC_I2S, SPI_I2S_FLAG_TXE))
+            	{
+                   for(i=0; i<45; i++)
+                   {
+                         SPI_I2S_SendData(CODEC_I2S, sample);
+                         sample = tab[i];
+                   }
+            	}
+            }
+            }
             f_close(&fil);
+
         }
         f_mount(0, "", 1);
     }
 
-    while(1)
-        {
-
-        	if (SPI_I2S_GetFlagStatus(CODEC_I2S, SPI_I2S_FLAG_TXE))
-        	{
-        		SPI_I2S_SendData(CODEC_I2S, sample);
-
-        		//only update on every second sample to insure that L & R ch. have the same sample value
-        		if (sampleCounter & 0x00000001)
-        		{
-        			sawWave += NOTEFREQUENCY;
-        			if (sawWave > 1.0)
-        				sawWave -= 2.0;
-
-        			filteredSaw = updateFilter(&filt, sawWave);
-
-        			sample = (int16_t)(NOTEAMPLITUDE*filteredSaw);
-        		}
-        		sampleCounter++;
-        	}
-
-        	if (sampleCounter==48000)
-        	{
-        		LED_BLUE_OFF;
-
-        	}
-        	else if (sampleCounter == 96000)
-        	{
-        		LED_BLUE_ON;
-        		sampleCounter = 0;
-        	}
-
-        }
-    }
-
-    // a very crude FIR lowpass filter
-float updateFilter(fir_8* filt, float val)
-{
-    uint16_t valIndex=0;
-    uint16_t paramIndex;
-    float outval = 0.0;
-
-    valIndex = filt->currIndex;
-    filt->tabs[valIndex] = val;
-
-    for (paramIndex=0; paramIndex<10; paramIndex++)
-    {
-    	outval += (filt->params[paramIndex]) * (filt->tabs[(valIndex+paramIndex)&0x07]); //??
-    }
-
-    valIndex++;
-    //valIndex &= 0x07; ??
-
-    filt->currIndex = valIndex;
-
-    return outval;
-}
-
-void initFilter(fir_8* theFilter)
-{
-    uint8_t i,k=0.1;
-
-    theFilter->currIndex = 0;
-
-    for (i=0; i<10; i++)
-    	theFilter->tabs[i] += k;
-
-}
